b13aff5a428ea682dcdec5c8ed040d80d0e1f193
livihades <stzesiades@gmail.com>
2020-03-16 17:50:27 +0200

    Initial branch Commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,16 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
 
 public class Controller {
+
+
 
 }
